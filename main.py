56e851dfffaef42f1bc5332bd10e69523f5a4d0b
SaulGonzalez81 <sgonz081@ucr.edu>
2021-03-15 02:24:17 -0700

    Fixed some range issues and got the cross validation working to a degree

--- main.py
+++ main.py
@@ -15,49 +15,52 @@ def leave_one_out_cross_validation(data,set_of_features,feature):
     #Deepcopy the data into a new dataframe so that we can make changes without affecting the orignal
     test_dataset = copy.deepcopy(data)
     for j in range(1,len(test_dataset.columns)):
-        if j not in set_of_features or j != feature:
-            test_dataset =test_dataset.drop([(j)])
+        if j not in set_of_features and j != feature:
+            test_dataset =test_dataset.drop(columns=[j])
     
 
     #number of correctly classfied entires
     number_correctly_classfied = 0
 
     #Going through all the entries and using their features to see what object they classify as
-    for i in range(1,len(test_dataset)-1):
-        object_to_classify = test_dataset.loc[1:len(test_dataset.columns)-1]
-        label_object_to_classify = test_dataset.iloc[i,1]
+    for i in range(len(test_dataset)):
+        object_to_classify = test_dataset.iloc[i,1:]
+        label_object_to_classify = test_dataset.at[i,0]
 
-        nearest_neighbor_distance = 0
+
+        nearest_neighbor_distance = 100
         nearest_neighbor_location = 0
         nearest_neighbor_label = 0
 
-        for k in range(1,len(test_dataset.columns-1)):
+        for k in range(len(test_dataset.columns)):
             if k != i:
-                distance = math.sqrt(sum(object_to_classify - pow((test_dataset.loc[1:len(test_dataset.columns) - 1]),2)))
+                distance = math.sqrt(sum(pow((object_to_classify - test_dataset.iloc[k,1:]),2)))
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
-                    nearest_neighbor_label = data.iloc[nearest_neighbor_location,1]
+                    nearest_neighbor_label = test_dataset.at[nearest_neighbor_location,0]
+
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classfied = number_correctly_classfied + 1
 
 
     #returning the accuracy of using said new feature
-    return (number_correctly_classfied / (len(test_dataset)-1))
+    return (number_correctly_classfied / (len(test_dataset)))
 
 
 def search_feature_function(data):
     #set of current features, intialized to zero at the start
     current_set_of_features = set()
-    for i in range(1,len(data)-1):
+    for i in range(len(data.columns)):
         print('On the ', str(i), 'th level of the search tree')
         best_accuracy_so_far = 0
         feature_to_add_at_this_level = 0
-        for k in range(1,len(data.columns)-1):
+        for k in range(1,len(data.columns)):
             if k not in current_set_of_features:
                 print('--Considering adding the ', str(k), ' feature')
-                #accuracy = leave_one_out_cross_validation(data,current_set_of_features,k)
-                accuracy = randint(1,10)
+                accuracy = leave_one_out_cross_validation(data,current_set_of_features,k)
+                #accuracy = randint(1,10)
+                
                 if accuracy > best_accuracy_so_far:
                     print("Change in accuracy\n")
                     best_accuracy_so_far = accuracy
@@ -69,3 +72,7 @@ def search_feature_function(data):
 
 ret = search_feature_function(main_data)
 print(ret)
+#test_set = set()
+#test_set.add(2)
+#num = leave_one_out_cross_validation(main_data,test_set,4)
+#print(num)
