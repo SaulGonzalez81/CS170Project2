6328dcfa08dbe85250d3ce5ec26a9ec065ea6b27
SaulGonzalez81 <sgonz081@ucr.edu>
2021-03-15 23:21:24 -0700

    Complete?

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from random import randint
 small_dataset = 'CS170_SMALLtestdata__21.txt'
 large_dataset = 'CS170_largetestdata__21.txt'
 special_dataset = 'CS170_small_special_testdata__95.txt'
-main_data = pd.read_table(special_dataset, delim_whitespace=True, header=None).values
+special_dataset2 = 'CS170_small_special_testdata__96.txt'
+#main_data = pd.read_table(special_dataset, delim_whitespace=True, header=None).values
 
 
 def leave_one_out_cross_validation(data,set_of_features,feature):
@@ -30,8 +31,7 @@ def leave_one_out_cross_validation(data,set_of_features,feature):
         object_to_classify = test_dataset[i,1:]
         label_object_to_classify = test_dataset[i,0]
 
-
-        nearest_neighbor_distance = 100
+        nearest_neighbor_distance = 1000
         nearest_neighbor_location = 0
         nearest_neighbor_label = 0
 
@@ -60,12 +60,12 @@ def search_feature_function(data):
     best_accuracy = 0
 
     for i in range(data.shape[1]):
-        print('On the ', str(i), 'th level of the search tree')
+        print('On the ', str(i), ' level of the search tree')
         best_accuracy_so_far = 0
         feature_to_add_at_this_level = 0
         for k in range(1,data.shape[1]):
             if k not in current_set_of_features:
-                print('--Considering adding the ', str(k), ' feature')
+                print('--Considering adding ', str(k), ' feature')
                 accuracy = leave_one_out_cross_validation(data,current_set_of_features,k)
                 
                 if accuracy > best_accuracy_so_far:
@@ -76,12 +76,67 @@ def search_feature_function(data):
         if best_accuracy_so_far > best_accuracy:
             best_set_of_features = copy.deepcopy(current_set_of_features)
             best_accuracy = best_accuracy_so_far
-            print('Best accuracy overall',best_accuracy)
-            print('Best set of features is: ', best_set_of_features)
-        
-        print('On this level ', str(i), ', i added feature ',str(feature_to_add_at_this_level))
-    print('Final Accuracy: ', best_accuracy)
-    print('Final set: ', best_set_of_features)
-    return best_set_of_features
+        if feature_to_add_at_this_level != 0:
+            print('On this level ', str(i), ', feature ',str(feature_to_add_at_this_level), ' was added.')
+        else:
+            print('On this level ', str(i), ', no feature was added.')
+    print('Final Best Accuracy: ', best_accuracy)
+    print('Final Best Set: ', best_set_of_features)  
 
-ret = search_feature_function(main_data)
+
+def backwards_elimination_search(data):
+    #set of current features, initialized to all of them from them
+    current_set_of_features = {i for i in range(1,data.shape[1])}
+
+    #set of features that create the best accuracy 
+    best_set_of_features = {i for i in range(1,data.shape[1])}
+    best_accuracy = 0
+
+
+    for i in range(data.shape[1]):
+        print('On the ', str(10-i), ' level of the search tree')
+        best_accuracy_so_far = 0
+        feature_to_eliminate_at_this_level = 0
+        for k in range(1,data.shape[1]):
+            if k in current_set_of_features:
+                print('--Considering deleting ', str(k), ' feature')
+                elim_set_data = copy.deepcopy(current_set_of_features)
+                elim_set_data.remove(k)
+                accuracy = leave_one_out_cross_validation(data,elim_set_data,0)
+
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_eliminate_at_this_level = k
+                    print("Change in accuracy, new accuracy: ",accuracy,"\n")
+        if feature_to_eliminate_at_this_level != 0:            
+            current_set_of_features.remove(feature_to_eliminate_at_this_level)
+            if best_accuracy_so_far > best_accuracy:
+                best_set_of_features = copy.deepcopy(current_set_of_features)
+                best_accuracy = best_accuracy_so_far
+            print('On this level ', str(10-i), ', feature ',str(feature_to_eliminate_at_this_level), ' was eliminated.')
+        else:
+            print('On this level ', str(10-i), ', no feature was eliminated.')
+    print('Final Best Accuracy: ', best_accuracy)
+    print('Final Best Set: ', best_set_of_features)    
+
+def main_function():
+    print("Hello! ¯\_(ツ)_/¯ Welcome to Bertie Woosters Feature Selection Algorithm!")
+    file_input = input('Type in the name of the file to test: ')
+    print('\nType the number of the algorithm you want to run.\n')
+    print('\t1) Forward Selection\n\t2) Backward Elimination\n')
+    search_input = input()
+    option = True
+    while option:
+        if search_input == str(1):
+            main_data = pd.read_table(file_input, delim_whitespace=True, header=None).values
+            search_feature_function(main_data)
+            option = False
+        elif search_input == str(2):
+            main_data = pd.read_table(file_input, delim_whitespace=True, header=None).values
+            backwards_elimination_search(main_data)
+            option = False
+        else:
+            print('Please enter a valid option! :)\n')
+            search_input = input()
+            option = True
+main_function()
